d4bda0378be74ce9418713bcc5b657b406df21ba
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-10-13 11:24:23 -0500

    added logging service

---
+++ LoggingCS/LogInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngine.Logging
+{
+    public record LogInformation(LogLevel Loglevel, string Module, string Message, DateTime Now, int ThreadID, string ThreadName);
+}
+
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { };
+}
