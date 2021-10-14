ef21a402446f543c41c2018f54413da9e32290df
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-10-13 19:51:20 -0500

    added dockerfile

---
+++ OrdersCS/ModifyOrderStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngine.Orders
+{
+    public class ModifyOrderStatus
+    {
+    }
+}
---
+++ OrdersCS/RejectCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngine.Orders
+{
+    public sealed class RejectCreator
+    {
+        public static Rejection GenerateOrderCoreRejection(IOrderCore rejectedOrder, RejectionReason rejectionReason)
+        {
+            return new Rejection(rejectedOrder, rejectionReason);
+        }
+    }
+}
