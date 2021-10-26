35f41705c2814f1f484bffba839bada2e3cca12c
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-10-25 20:19:24 -0500

    added orderbook

---
+++ SophiaCS/IReadOnlyOrderbook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TradingEngine.Orderbook
+{
+    public interface IReadOnlyOrderbook
+    {
+        bool ContainsOrder(long orderId);
+        OrderbookSpread GetSpread();
+        int Count { get;  }
+    }
+}
---
+++ SophiaCS/IRetrievalOrderbook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingEngine.Orders;
+
+namespace TradingEngine.Orderbook
+{
+    interface IRetrievalOrderbook : IOrderEntryOrderbook
+    {
+        List<OrderbookEntry> GetAskOrders();
+        List<OrderbookEntry> GetBidOrders();
+    }
+}
